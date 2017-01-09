badc49d829fbbc57a5dfeb32ab81b057cc390913
Daleth <i.publish.docs@gmail.com>
2017-01-09 13:13:44 -0800

    fixed merge conflicts from stashing

--- general_base/impl/Precision_Signed_Interface.h
+++ general_base/impl/Precision_Signed_Interface.h
@@ -7,21 +7,31 @@
 #ifndef S111111IGN335_TYPE_NUMBER_I66NTERFACE__________HHHHHH______
 #define S111111IGN335_TYPE_NUMBER_I66NTERFACE__________HHHHHH______
 
+#include "Precision_Sign_Class.h"
+
 namespace Precision{
-    /** Precision::Signed_Integer provides a representation for a number
-     *  sign such that all number types can store a number's magnitude
-     *  and numerical sign separately. This class also simplifies sign
-     *  related operations and follows these rules:
+    /** Precision::Signed_Interface provides a set of functions for
+     *  manipulating the numerical sign that can be inherited, rather than
+     *  having the client classes redefine the same functions over and over
+     *  again. This class also serves as a thin layer over
+     *  Precision::SignClass such that child classes still follow a
+     *  composition structure and can pass a sign object to functions
+     *  without having to pass a reference to the whole class, which would
+     *  not make sense in situations where only a sign is required.
+     *  
+     *  This class obeys the same rules as Precision::SignClass as follows:
      *      - Negative == -1
      *      - Positive == +1
      *  For purposes of conversion, positive includes [0, inf) and
      *  negative includes (-inf, 0).
      */
-    class Signed_Integer{
+    class Signed_Interface{
         public:
             // Type-aliases
-            /** signed_type represents the output type that can be +1 or -1 */
-            using sign_type = short;
+            /** sign_type represents the underlying sign class used for
+             *  composition. The class is assumed to be a lightweight POD.
+             */
+            using sign_type = Precision::SignClass;
 
             // Read-only
             /** Return if the sign is positive, or +1.
@@ -29,21 +39,22 @@ namespace Precision{
              * \return Whether the sign is considered positive.
              */
             bool is_positive()const
-                {return !m_negative;}
+                {return m_sign.is_positive();}
 
             /** Return if the sign is negative, or -1.
              *
              * \return Whether the sign is considered negative.
              */
             bool is_negative()const
-                {return m_negative;}
+                {return m_sign.is_negative();}
 
             /** Return the actual sign value itself.
              *
-             * \return -1 for negative and +1 for positive.
+             * \return The sign object representing the value
+             *         (-1 for negative and +1 for positive).
              */
             sign_type sign()const
-                {return 1 - m_negative * 2;}
+                {return m_sign;}
 
 
 
@@ -52,15 +63,15 @@ namespace Precision{
              *  or vice versa.
              */
             void negate()
-                {m_negative = !m_negative;}
+                {m_sign.negate();}
 
             /** Force the sign to be positive. */
             void make_positive()
-                {m_negative = false;}
+                {m_sign.make_positive();}
 
             /** Force the sign to be negative. */
             void make_negative()
-                {m_negative = true;}
+                {m_sign.make_negative();}
 
             /** Change the sign to positive or negative.
              *
@@ -69,7 +80,7 @@ namespace Precision{
              *                  [0, inf) --> Positive
              */
             void sign(sign_type new_sign)
-                {m_negative = new_sign < 0;}
+                {m_sign.assign(new_sign);}
 
 
 
@@ -81,25 +92,25 @@ namespace Precision{
              *                  (-inf, 0) --> Negative
              *                  [0, inf) --> Positive
              */
-            Signed_Integer(sign_type new_sign)
-                : m_negative(new_sign < 0)
+            Signed_Interface(sign_type new_sign)
+                : m_sign(new_sign)
             {}
 
             /** Compiler generated default constructor. */
-            Signed_Integer()                                 = default;
+            Signed_Interface()                                   = default;
             /** Compiler generated copy constructor. */
-            Signed_Integer(const Signed_Integer&)            = default;
+            Signed_Interface(const Signed_Interface&)            = default;
             /** Compiler generated copy move constructor. */
-            Signed_Integer(Signed_Integer&&)                 = default;
+            Signed_Interface(Signed_Interface&&)                 = default;
             /** Compiler generated assignment operator. */
-            Signed_Integer& operator=(const Signed_Integer&) = default;
+            Signed_Interface& operator=(const Signed_Interface&) = default;
             /** Compiler generated move assignment operator. */
-            Signed_Integer& operator=(Signed_Integer&&)      = default;
+            Signed_Interface& operator=(Signed_Interface&&)      = default;
             /** Compiler generated destructor. */
-            ~Signed_Integer()                                = default;
+            ~Signed_Interface()                                  = default;
 
         protected:
-            bool m_negative;
+            sign_type m_sign;
     };
 }
 
